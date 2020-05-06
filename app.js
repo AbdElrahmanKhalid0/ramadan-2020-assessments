17469097079f9cacc1fdeda9cb8495f2c0cb5b24
Abdelrahman Khalid <abdelrahman.elzohry2014@gmail.com>
2020-05-06 09:38:42 +0200

    added the functionality to vote

--- app.js
+++ app.js
@@ -1,31 +1,35 @@
 const getCard = ({topic_title,topic_details,expected_result,votes,status,author_name,submit_date,target_level}) => {
-    return `<div class="card-body d-flex justify-content-between flex-row">
-                <div class="d-flex flex-column">
-                <h3>${topic_title}</h3>
-                <p class="text-muted mb-2">${topic_details}</p>
-                <p class="mb-0 text-muted">
-                    ${expected_result && `<strong>Expected results:</strong> ${expected_result}`}
-                </p>
-                </div>
-                <div class="d-flex flex-column text-center">
-                <a class="btn btn-link">🔺</a>
-                <h3>${votes.ups - votes.downs}</h3>
-                <a class="btn btn-link">🔻</a>
-                </div>
+    const videoCard = document.createElement('div');
+    videoCard.classList.add('card','mb-3');
+    videoCard.innerHTML = 
+        `<div class="card-body d-flex justify-content-between flex-row">
+            <div class="d-flex flex-column">
+            <h3>${topic_title}</h3>
+            <p class="text-muted mb-2">${topic_details}</p>
+            <p class="mb-0 text-muted">
+                ${expected_result && `<strong>Expected results:</strong> ${expected_result}`}
+            </p>
             </div>
-            <div class="card-footer d-flex flex-row justify-content-between">
-                <div>
-                <span class="text-info">${status.toUpperCase()}</span>
-                &bullet; added by <strong>${author_name}</strong> on
-                <strong>${new Date(submit_date).toDateString()}</strong>
-                </div>
-                <div
-                class="d-flex justify-content-center flex-column 408ml-auto mr-2"
-                >
-                <div class="badge badge-success">
-                    ${target_level}
-                </div>
-            </div>`
+            <div class="d-flex flex-column text-center">
+            <a class="btn btn-link">🔺</a>
+            <h3>${votes.ups - votes.downs}</h3>
+            <a class="btn btn-link">🔻</a>
+            </div>
+        </div>
+        <div class="card-footer d-flex flex-row justify-content-between">
+            <div>
+            <span class="text-info">${status.toUpperCase()}</span>
+            &bullet; added by <strong>${author_name}</strong> on
+            <strong>${new Date(submit_date).toDateString()}</strong>
+            </div>
+            <div
+            class="d-flex justify-content-center flex-column 408ml-auto mr-2"
+            >
+            <div class="badge badge-success">
+                ${target_level}
+            </div>
+        </div>`;
+    return videoCard;
 };
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -37,10 +41,37 @@ document.addEventListener('DOMContentLoaded', () => {
         .then(result => result.json())
         .then(videoRequests => {
             videoRequests.map(videoRequest => {
-                const videoCard = document.createElement('div');
-                videoCard.classList.add('card','mb-3');
-                videoCard.innerHTML = getCard(videoRequest);
-                requestsList.appendChild(videoCard)
+                const videoRequestCard = getCard(videoRequest);
+
+                voteUp = videoRequestCard.querySelectorAll('a')[0];
+                voteUp.addEventListener('click', () => {
+                    fetch('http://localhost:7777/video-request/vote', {
+                        method:'PUT',
+                        headers:{
+                            'Content-Type':'application/json'
+                        },
+                        body:JSON.stringify({
+                            id: videoRequest._id,
+                            vote_type: 'ups'
+                        })
+                    });
+                });
+                
+                voteDown = videoRequestCard.querySelectorAll('a')[1];
+                voteDown.addEventListener('click', () => {
+                    fetch('http://localhost:7777/video-request/vote', {
+                        method:'PUT',
+                        headers:{
+                            'Content-Type':'application/json'
+                        },
+                        body:JSON.stringify({
+                            id: videoRequest._id,
+                            vote_type: 'downs'
+                        })
+                    });
+                });
+                
+                requestsList.appendChild(videoRequestCard);
             })
         })
 
@@ -66,10 +97,7 @@ document.addEventListener('DOMContentLoaded', () => {
         })
         .then(res => res.json())
         .then(videoData => {
-            const videoCard = document.createElement('div');
-            videoCard.classList.add('card','mb-3');
-            videoCard.innerHTML = getCard(videoData);
-            requestsList.prepend(videoCard);
+            requestsList.prepend(getCard(videoData));
         })
         
     });
